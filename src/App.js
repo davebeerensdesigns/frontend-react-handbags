470257f60db853547b490ce3a1675ec708895020
davebeerensdesigns <84903154+davebeerensdesigns@users.noreply.github.com>
2022-05-02 11:37:22 +0200

    product comp

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import Bag1 from './assets/bag_1.png';
+import Bag2 from './assets/bag_2.png';
+import Bag3 from './assets/bag_3.png';
+import Bag4 from './assets/bag_4.png';
 import Menu from "./Menu";
+import Product from "./Product";
 import './App.css';
 
 function App() {
@@ -21,6 +26,32 @@ function App() {
                     logmessage="pre-orders"
                 />
             </nav>
+            <main>
+                <Product
+                    label="Best seller"
+                    img={Bag1}
+                    title="The handy bag"
+                    price="400,-"
+                />
+                <Product
+                    label="Best seller"
+                    img={Bag2}
+                    title="The stylish bag"
+                    price="250,-"
+                />
+                <Product
+                    label="New collection"
+                    img={Bag3}
+                    title="The simple bag"
+                    price="300,-"
+                />
+                <Product
+                    label="New collection"
+                    img={Bag4}
+                    title="The trendy bag"
+                    price="150,-"
+                />
+            </main>
         </>
     );
 }
